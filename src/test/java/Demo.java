3f7020b56bbbfbf8043d0b2702239529fa82bb9c
iceBear67 <icebear67@sfclub.cc>
2021-10-13 20:06:29 +0800

    Fix JITPack

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -4,7 +4,7 @@ import io.ib67.SimpleChatColor;
 public class Demo {
     public static void main(String[] args) throws InterruptedException {
         //System.out.println(parseAnnouncementAddressPort(new ServerEntry("motdaaa",null,-1).toString()));
-        DiscoverMe.INSTANCE.addEntry(SimpleChatColor.randomColorize("NullCat 女生自用服务器 | 女装 嘿客 湿法炼铜"), 114514);
+        DiscoverMe.INSTANCE.addEntry(SimpleChatColor.randomColorize("NullCat 女生自用服务器 | 女装 嘿客 湿法炼铜"));
         while (true) Thread.sleep(1000);
     }
 }
